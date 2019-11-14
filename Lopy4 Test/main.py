2aadb40221a35a72e14320de724c3e57ce59c4de
Noobzie <mohaas121@hotmail.com>
2019-11-14 14:47:37 +0100

    Updated description

--- Lopy4 Test/main.py
+++ Lopy4 Test/main.py
@@ -2,11 +2,12 @@
 
 from machine import Pin, I2C
 import time
-import lib.CCS811
+import CCS811
 import pycom
 
 
 def main():
+    print('Starting application...')
     i2c = I2C(scl=Pin(14), sda=Pin(13))
     # Adafruit sensor breakout has i2c addr: 90; Sparkfun: 91
     s = CCS811.CCS811(i2c=i2c, addr=90)
